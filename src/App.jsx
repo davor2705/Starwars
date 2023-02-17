a316050ecb56fdd981a5fc410827dd118d96a6cf
Davor <hyperv@ucione.local>
2023-02-17 20:35:34 +0100

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { Route, Routes } from "react-router-dom";
 import Nav from "./components/Nav";
 import Home from "./pages/Home";
+import About from "./pages/About";
+import Shop from "./pages/Shop";
 
 const App = () => {
   return (
     <div>
       <Routes>
             <Route path='/' element={<Nav />}>
-                <Route path='' element={<Home />} /> 
+                <Route index element={<Home />} /> 
+                <Route path='about' element={<About />} /> 
+                <Route path='shop' element={<Shop />} /> 
             </Route>
       </Routes>
     </div>
